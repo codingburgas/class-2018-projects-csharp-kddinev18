33aac822da8a2800a4602bc9952b55970e83d0b9
kddinev18 <kddinev18@codingburgas.bg>
2022-05-04 19:50:35 +0300

    Create DiabetesTracker database and Users table

---
+++ DiabetesTracker/Models/DiabetesTrackerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiabetesTracker.Models
+{
+    public class DiabetesTrackerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=DiabetesTracker;Integrated Security=True;");
+            }
+        }
+    }
+}
