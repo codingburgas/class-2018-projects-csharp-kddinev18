a252efeaaf74d21b2892ce6331b8d810f260a8fd
kddinev18 <kddinev18@codingburgas.bg>
2022-05-04 19:33:30 +0300

    Add User class representing Users table

---
+++ DiabetesTracker/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiabetesTracker.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Salt { get; set; }
+        public DateTime DateRegistered { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool AccountStatus { get; set; }
+    }
+}
