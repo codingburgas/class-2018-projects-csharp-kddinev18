903d683056c48a2d2bb1c315af64482be33c8181
kddinev18 <kddinev18@codingburgas.bg>
2022-05-06 23:14:20 +0300

    Implmrnt Follow() in FollowingBlog class

--- DiabetesTracker/Models/FollowingBlog.cs
+++ DiabetesTracker/Models/FollowingBlog.cs
@@ -9,9 +9,20 @@ namespace DiabetesTracker.Models
     {
         public int BlogId { get; set; }
         public int UserId { get; set; }
-        public int FollowedOn { get; set; }
+        public DateTime FollowedOn { get; set; }
 
-        public virtual User FollowedOnNavigation { get; set; }
-        public virtual Blog User { get; set; }
+        public virtual User User { get; set; }
+        public virtual Blog Blog { get; set; }
+
+        public static void Follow(DiabetesTrackerDbContext dbContext, Blog blog)
+        {
+            dbContext.FollowingBlogs.Add(new FollowingBlog() 
+            {
+                BlogId = blog.BlogId,
+                UserId = User.GetCurrentUser(),
+            });
+
+            dbContext.SaveChanges();
+        }
     }
 }
