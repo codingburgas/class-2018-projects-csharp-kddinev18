2a6adfb7bd1754572ec416b9d394ec2fb653724a
kddinev18 <kddinev18@codingburgas.bg>
2022-05-08 00:50:38 +0300

    Add finish registration page

---
+++ DiabetesTracker/ViewModels/FinishRegistrationForm.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiabetesTracker.ViewModels
+{
+    /// <summary>
+    /// Interaction logic for FinishRegistrationForm.xaml
+    /// </summary>
+    public partial class FinishRegistrationForm : Page
+    {
+        public FinishRegistrationForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void FinishRegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
