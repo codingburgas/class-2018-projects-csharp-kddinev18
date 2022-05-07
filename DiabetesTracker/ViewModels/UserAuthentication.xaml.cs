8961880377dbcd4f47413430be2a4727119a32c5
kddinev18 <kddinev18@codingburgas.bg>
2022-05-07 12:06:47 +0300

    Add basic strucure to the UserAuthentication page

--- DiabetesTracker/ViewModels/UserAuthentication.xaml.cs
+++ DiabetesTracker/ViewModels/UserAuthentication.xaml.cs
@@ -10,18 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using DiabetesTracker.Models;
 
-namespace DiabetesTracker
+namespace DiabetesTracker.ViewModels
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UserAuthentication.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserAuthentication : Window
     {
-        public MainWindow()
+        public UserAuthentication()
         {
             InitializeComponent();
         }
