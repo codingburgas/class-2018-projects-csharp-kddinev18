6071e6de529cf82928ad6ed11663e7b3e6d49ff0
kddinev18 <kddinev18@codingburgas.bg>
2022-05-08 20:01:16 +0300

    Implement RememberMe functionality

--- DiabetesTracker/ViewModels/LogInForm.xaml.cs
+++ DiabetesTracker/ViewModels/LogInForm.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using DiabetesTracker.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,12 +22,24 @@ namespace DiabetesTracker.ViewModels
     /// <summary>
     /// Interaction logic for LogInForm.xaml
     /// </summary>
+    public class UserCredentials
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
     public partial class LogInForm : Page
     {
         private DiabetesTrackerDbContext _dbContext;
         private UserAuthentication _userAuthentication;
+        private readonly string _userCredentialsPath = @$"C:\Users\{WindowsIdentity.GetCurrent().Name}\AppData\Local\Microsoft\Windows\INetCookies\DiabetesTracker.txt";
         public LogInForm(DiabetesTrackerDbContext dbContext, UserAuthentication userAuthentication)
         {
+            if(Directory.Exists(_userCredentialsPath))
+            {
+                string credentials = File.ReadAllText(_userCredentialsPath);
+                UserCredentials userCredentials = JsonSerializer.Deserialize<UserCredentials>(credentials);
+                User.LogIn(_dbContext, userCredentials.UserName, userCredentials.Password);
+            }
             _userAuthentication = userAuthentication;
             _dbContext = dbContext;
             InitializeComponent();
@@ -46,6 +61,12 @@ namespace DiabetesTracker.ViewModels
 
             if (User.CheckUserProfile(_dbContext, User.GetCurrentUser()) == false)
                 _userAuthentication.ShowFinishRegisterForm();
+
+            if(RememberMeCheckBox.IsChecked == true)
+            {
+                Directory.CreateDirectory(_userCredentialsPath);
+                File.WriteAllText(_userCredentialsPath, JsonSerializer.Serialize(new UserCredentials() { UserName = userName, Password = password}));
+            }
         }
     }
 }
