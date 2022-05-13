ab925e673ad461dffd708a54cc6e27792f881a87
kddinev18 <kddinev18@codingburgas.bg>
2022-05-13 22:31:29 +0300

    Extraxt Social media tab into a page

--- DiabetesTracker/ViewModels/MainWindow/Pages/SocialMediaPage.xaml.cs
+++ DiabetesTracker/ViewModels/MainWindow/Pages/SocialMediaPage.xaml.cs
@@ -10,23 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DiabetesTracker.ViewModels
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SocialMediaPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SocialMediaPage : Page
     {
-        public MainWindow()
+        public SocialMediaPage()
         {
             InitializeComponent();
-            MainWindowFrame.Content = new SocialMediaPage();
-        }
-        private void ExitButton_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
         }
     }
 }
