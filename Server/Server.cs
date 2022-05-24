3c9b66cdcff27ff67dfbf8ff9db9c6eeb8eef90a
kddinev18 <kddinev18@codingburgas.bg>
2022-05-24 17:56:14 +0300

    Implement ServerSertUp()

--- Server/Server.cs
+++ Server/Server.cs
@@ -10,6 +10,20 @@ namespace Server
 {
     public class Server
     {
+        private static TcpListener _tcpListener;
+        private static List<TcpClient> _clients = new List<TcpClient>();
+        private static byte[] _data = new byte[128];
+        private int _port;
+        public Server(int port)
+        {
+            _port = port;
+        }
 
+        public void ServerSertUp()
+        {
+            _tcpListener = new TcpListener(IPAddress.Any, _port);
+            _tcpListener.Start();
+            _tcpListener.BeginAcceptTcpClient(new AsyncCallback(AcceptClients), null);
+        }
     }
 }
