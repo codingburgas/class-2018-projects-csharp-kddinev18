83b3b7b7d40b189adc74ca43d3aee2c45bb3625c
kddinev18 <kddinev18@codingburgas.bg>
2022-05-24 17:56:42 +0300

    Implement AcceptClients() in Server

--- Server/Server.cs
+++ Server/Server.cs
@@ -25,5 +25,24 @@ namespace Server
             _tcpListener.Start();
             _tcpListener.BeginAcceptTcpClient(new AsyncCallback(AcceptClients), null);
         }
+
+        public static void AcceptClients(IAsyncResult asyncResult)
+        {
+            TcpClient client;
+            try
+            {
+                client = _tcpListener.EndAcceptTcpClient(asyncResult);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            _clients.Add(client);
+            client.Client.Send(Encoding.ASCII.GetBytes("HI"));
+            client.Client.BeginReceive(_data, 0, _data.Length, SocketFlags.None, new AsyncCallback(ReciveUserInput), client);
+            _tcpListener.BeginAcceptTcpClient(new AsyncCallback(AcceptClients), null);
+        }
     }
 }
