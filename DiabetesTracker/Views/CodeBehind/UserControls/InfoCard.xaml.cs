92348d7e98832762d33d3c42dc722a90124cadf1
kddinev18 <kddinev18@codingburgas.bg>
2022-05-29 19:38:23 +0300

    Add info card userControl

---
+++ DiabetesTracker/Views/CodeBehind/UserControls/InfoCard.xaml.cs
@@ -0,0 +1,57 @@
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiabetesTracker.ViewModels.UserControls
+{
+    /// <summary>
+    /// Interaction logic for InfoCard.xaml
+    /// </summary>
+    public partial class InfoCard : UserControl
+    {
+        public InfoCard()
+        {
+            InitializeComponent();
+        }
+
+        public PackIconMaterialKind InfoCardIcon
+        {
+            get { return (PackIconMaterialKind)GetValue(InfoCardIconProperty); }
+            set { SetValue(InfoCardIconProperty, value); }
+        }
+
+        public static readonly DependencyProperty InfoCardIconProperty =
+            DependencyProperty.Register("InfoCardIcon", typeof(PackIconMaterialKind), typeof(InfoCard));
+
+
+        public string InfoCardTitle
+        {
+            get { return (string)GetValue(InfoCardTitleProperty); }
+            set { SetValue(InfoCardTitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty InfoCardTitleProperty =
+            DependencyProperty.Register("InfoCardTitle", typeof(string), typeof(InfoCard));
+
+        public string InfoCardText
+        {
+            get { return (string)GetValue(InfoCardTextProperty); }
+            set { SetValue(InfoCardTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty InfoCardTextProperty =
+            DependencyProperty.Register("InfoCardText", typeof(string), typeof(InfoCard));
+    }
+}
