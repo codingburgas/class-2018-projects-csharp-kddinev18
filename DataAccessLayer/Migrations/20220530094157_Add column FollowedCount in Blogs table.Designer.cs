2822bd0a8660f9cf0c8c866f66581776a170ab50
kddinev18 <kddinev18@codingburgas.bg>
2022-05-30 12:48:34 +0300

    Fix a bug involving conversion between integer and strings

--- DataAccessLayer/Migrations/20220530094157_Add column FollowedCount in Blogs table.Designer.cs
+++ DataAccessLayer/Migrations/20220530094157_Add column FollowedCount in Blogs table.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DiabetesTracker.Migrations
 {
     [DbContext(typeof(DiabetesTrackerDbContext))]
-    partial class DiabetesTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220530094157_Add column FollowedCount in Blogs table")]
+    partial class AddcolumnFollowedCountinBlogstable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
