ec61d9f7e16a38e6830798d6d01ae93221252784
kddinev18 <kddinev18@codingburgas.bg>
2022-06-08 19:57:29 +0300

    Add project for unit testing

---
+++ DiabetesTracker_Tests/BusinessLogicTest .cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DiabetesTracker_Tests
+{
+    public class BusinessLogicTest 
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
