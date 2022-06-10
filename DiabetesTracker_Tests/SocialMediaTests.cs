eed9cdbd41f8cd695e92cc3c613f1cfeeddbe7f3
kddinev18 <kddinev18@codingburgas.bg>
2022-06-10 22:37:41 +0300

    Combined SocialMediaLogic test into SocialMediaTests

--- DiabetesTracker_Tests/SocialMediaTests.cs
+++ DiabetesTracker_Tests/SocialMediaTests.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace DiabetesTracker_Tests
 {
-    public class PostLogicTests
+    public class SocialMediaTests
     {
         private DiabetesTrackerDbContext _dBContext;
         private User _testUser;
@@ -65,6 +65,121 @@ namespace DiabetesTracker_Tests
         }
 
         [Test]
+        public void Test_Master_OpenConnection()
+        {
+            Assert.That(_dBContext is not null);
+        }
+
+        [Test]
+        public void Test_Master_CloseConnection()
+        {
+            Master.OpenConnection();
+            Master.CloseConnection();
+
+            Assert.IsNull(BlogLogic.DbContext);
+        }
+
+
+        [Test]
+        public void Test_BlogLogic_CreateBlog()
+        {
+            Blog blog = _dBContext.Blogs.Where(blog => blog.BlogId == _testBlog.BlogId).First();
+
+            Assert.That(_testBlog.BlogId == blog.BlogId && _testBlog.Name == blog.Name);
+        }
+
+        [Test]
+        public void Test_BlogLogic_BelongsToUser()
+        {
+            Assert.That(BlogLogic.BelogsToUser(_testUser.UserId, _testBlog.BlogId) == true);
+        }
+
+        [TestCase("TestBlogName")]
+        public void Test_BlogLogic_GetBlogsByName(string name)
+        {
+            List<Blog> blogs = BlogLogic.GetBlogsByName(name);
+
+            Assert.That(blogs.Count >= 1);
+        }
+
+        [Test]
+        public void Test_BlogLogic_GetCurrentUserBlogs()
+        {
+            List<Blog> blogs = BlogLogic.GetCurrentUserBlogs(_testUser.UserId);
+
+            Assert.That(blogs.Count == 1);
+        }
+
+        [Test]
+        public void Test_BlogLogic_GetBlogPostsCount()
+        {
+            int postCount = BlogLogic.GetBlogPostsCount(_testBlog.BlogId);
+
+            Assert.That(postCount == 1);
+        }
+
+        [Test]
+        public void Test_BlogLogic_GetBlogFollowingCount()
+        {
+            int followingCount = BlogLogic.GetBlogFollowingCount(_testUser.UserId);
+
+            Assert.That(followingCount == 1);
+        }
+
+        [Test]
+        public void Test_BlogLogic_GetAllPosts()
+        {
+            List<Post> posts = BlogLogic.GetAllPosts(_testBlog.BlogId);
+
+            Assert.That(posts.Count == 1);
+        }
+
+        [Test]
+        public void Test_BlogLogic_GetLeastFollowedBlod()
+        {
+            string blogName = BlogLogic.GetLeastFollowedBlod(_testUser.UserId);
+
+            Assert.That(blogName == _testBlog.Name);
+        }
+
+        [Test]
+        public void Test_FollowingBlogLogic_Follow_Unfollow()
+        {
+            FollowingBlogLogic.Follow(_testUser.UserId, _testBlog.BlogId);
+            _dBContext.SaveChanges();
+
+            FollowingBlog followingBlog = _dBContext.FollowingBlogs.Where(followingBlog => followingBlog.BlogId == _testBlog.BlogId && followingBlog.UserId == _testUser.UserId).First();
+            FollowingBlogLogic.Unfollow(_testUser.UserId, _testBlog.BlogId);
+
+            Assert.IsNotNull(followingBlog);
+        }
+
+        [Test]
+        public void Test_FollowingBlogLogic_IsCurrentUserFollowed_Followed()
+        {
+            FollowingBlogLogic.Follow(_testUser.UserId, _testBlog.BlogId);
+            _dBContext.SaveChanges();
+
+            bool result = FollowingBlogLogic.IsCurrentUserFollowed(_testUser.UserId, _testBlog.BlogId);
+            FollowingBlogLogic.Unfollow(_testUser.UserId, _testBlog.BlogId);
+
+            Assert.That(result);
+
+        }
+
+        [Test]
+        public void Test_FavouritePostLogic_IsCurrentUserFavourited_NotFavourited()
+        {
+            Assert.That(FollowingBlogLogic.IsCurrentUserFollowed(_testUser.UserId, _testBlog.BlogId) == false);
+        }
+
+        [Test]
+        public void Test_FavouritePostLogic_GetFavouritePosts_NoFavouritedPosts()
+        {
+            Assert.That(FollowingBlogLogic.GetFollowingBlogs(_testUser.UserId).Count == 0);
+        }
+
+        [Test]
         public void Test_FavouritePostLogic_Favourite_Unfavourite()
         {
             FavouritePostLogic.Favourite(_testPost.PostId, _testUser.UserId);
@@ -86,18 +201,6 @@ namespace DiabetesTracker_Tests
 
             FavouritePostLogic.Unfavourite(_testPost.PostId, _testUser.UserId);
 
-        }
-
-        [Test]
-        public void Test_FavouritePostLogic_IsCurrentUserFavourited_NotFavourited()
-        {
-            Assert.That(FavouritePostLogic.IsCurrentUserFavourited(_testPost.PostId, _testUser.UserId) == false);
-        }
-
-        [Test]
-        public void Test_FavouritePostLogic_GetFavouritePosts_NoFavouritedPosts()
-        {
-            Assert.That(FavouritePostLogic.GetFavouritePosts(_testPost.PostId, _testUser.UserId).Count == 0);
         }
 
         [Test]
@@ -223,5 +326,7 @@ namespace DiabetesTracker_Tests
 
             Assert.That(postsCount >= 1);
         }
+
+
     }
 }
