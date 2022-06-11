334ff75c12d2bbca7729e28654875f86fb984b37
kddinev18 <kddinev18@codingburgas.bg>
2022-06-11 16:15:00 +0300

    Comment the front-end logic

--- DiabetesTracker/Logic/SocialMediaLogic.cs
+++ DiabetesTracker/Logic/SocialMediaLogic.cs
@@ -16,6 +16,7 @@ namespace DiabetesTracker.Logic
 {
     public static class SocialMediaLogic
     {
+        // Opens a file dopblog box and sets the selected item path
         public static BitmapImage ImagePickerDialog(ref string selectedImagePath)
         {
             // Create OpenFileDialog 
@@ -38,6 +39,8 @@ namespace DiabetesTracker.Logic
             else
                 return null;
         }
+
+        // Converts a byte array into a BitmapImage
         public static BitmapImage ConvertByteArrayToBitMapImage(byte[] imageByteArray)
         {
             BitmapImage img = new BitmapImage();
@@ -51,9 +54,14 @@ namespace DiabetesTracker.Logic
             }
             return img;
         }
+
+        // Load information into ObservableCollection<CurrentBlogInformation> YourBlogsInformation
         public static void LoadBlogsInformation(ObservableCollection<CurrentBlogInformation> YourBlogsInformation)
         {
+            // Get blogs belonging to the current user
             List<BlogInformation> currentUserblogsInformation = Services.GetBlogs(CurrentUserInformation.CurrentUserId.Value);
+
+            // For every blog in currentUserblogsInformation add CurrentBlogInformation in ObservableCollection<CurrentBlogInformation> YourBlogsInformation
             foreach (BlogInformation currentUserblogInformation in currentUserblogsInformation)
             {
                 YourBlogsInformation.Add(new CurrentBlogInformation()
@@ -67,10 +75,16 @@ namespace DiabetesTracker.Logic
                 });
             }
         }
+
+        // Load information into ObservableCollection<CurrentBlogInformation> SearchBlogsInformation
         public static void LoadBlogsInformation(string blogName, ObservableCollection<CurrentBlogInformation> SearchBlogsInformation)
         {
+            // Clear the list
             SearchBlogsInformation.Clear();
+
+            // Get blogs by name
             List<BlogInformation> searchBlogsInformation = Services.GetBlogs(CurrentUserInformation.CurrentUserId.Value, blogName);
+            // For every blog in searchBlogsInformation add CurrentBlogInformation in ObservableCollection<CurrentBlogInformation> SearchBlogsInformation
             foreach (BlogInformation searchBlogInformation in searchBlogsInformation)
             {
                 SearchBlogsInformation.Add(new CurrentBlogInformation()
@@ -84,9 +98,13 @@ namespace DiabetesTracker.Logic
                 });
             }
         }
+
+        // Load information into ObservableCollection<CurrentBlogInformation> FollowingBlogsInformation
         public static void LoadFollowedBlogsInformation(ObservableCollection<CurrentBlogInformation> FollowingBlogsInformation)
         {
+            // Get blogs followed by the current user
             List<BlogInformation> followingblogsInformation = Services.GetFollowingBlogs(CurrentUserInformation.CurrentUserId.Value);
+            // For every blog in followingblogsInformation add CurrentBlogInformation in ObservableCollection<CurrentBlogInformation> FollowingBlogsInformation
             foreach (BlogInformation followingblogInformation in followingblogsInformation)
             {
                 FollowingBlogsInformation.Add(new CurrentBlogInformation()
@@ -101,110 +119,159 @@ namespace DiabetesTracker.Logic
             }
         }
 
+        // Sets the next or previous post in a blog and returns the colors of the like and favourite button
         public static (SolidColorBrush, SolidColorBrush) SetPost(ref List<PostInformation> postsInformation, ref BlogContent blogContent, int index, int pagingCount)
         {
             (SolidColorBrush likeIconColor, SolidColorBrush favouriteIconColor) iconColors;
+
+            // If the next post is liked set the likeIconColor to DeepSkyBlue
             if (postsInformation[index % pagingCount].IsPostLiked == true)
             {
                 iconColors.likeIconColor = new SolidColorBrush(Colors.DeepSkyBlue);
             }
+            // Else set the likeIconColor to #2b2b2b
             else
             {
                 iconColors.likeIconColor = (SolidColorBrush)new BrushConverter().ConvertFrom("#2b2b2b");
             }
 
+            // If the next post is favourited set the favouriteIconColor to Red
             if (postsInformation[index % pagingCount].IsPostFavourited == true)
             {
                 iconColors.favouriteIconColor = new SolidColorBrush(Colors.Red);
             }
+            // Else set the favouriteIconColor to #2b2b2b
             else
             {
                 iconColors.favouriteIconColor = (SolidColorBrush)new BrushConverter().ConvertFrom("#2b2b2b");
             }
 
+            // Set the content of the post to the next post's content
             blogContent.PostContent = postsInformation[index % pagingCount].PostContent;
+            // Set the image of the post to the next post's image
             blogContent.PostImage = ConvertByteArrayToBitMapImage(postsInformation[index % pagingCount].PostImage);
 
+            // Returns the colors of the likeIcon and favouriteIcon
             return iconColors;
         }
+
+        // Sets the next or previous post and returns the colors of the like and favourite button
         public static (SolidColorBrush, SolidColorBrush) SetPost(ref List<PostInformation> postsInformation, ref CurrentPostInformation postInformation, int index, int pagingCount)
         {
             (SolidColorBrush likeIconColor, SolidColorBrush favouriteIconColor) iconColors;
 
+            // If the next post is liked set the likeIconColor to DeepSkyBlue
             if (postsInformation[index % pagingCount].IsPostLiked == true)
             {
                 iconColors.likeIconColor = new SolidColorBrush(Colors.DeepSkyBlue);
             }
+            // Else set the likeIconColor to #2b2b2b
             else
             {
                 iconColors.likeIconColor = (SolidColorBrush)new BrushConverter().ConvertFrom("#2b2b2b");
             }
 
+            // If the next post is favourited set the favouriteIconColor to Red
             if (postsInformation[index % pagingCount].IsPostFavourited == true)
             {
                 iconColors.favouriteIconColor = new SolidColorBrush(Colors.Red);
             }
+            // Else set the favouriteIconColor to #2b2b2b
             else
             {
                 iconColors.favouriteIconColor = (SolidColorBrush)new BrushConverter().ConvertFrom("#2b2b2b");
             }
 
+            // Set the blog name of the post to the next post's blog name
             postInformation.BlogName = postsInformation[index % pagingCount].BlogName;
+            // Set the content of the post to the next post's content
             postInformation.PostContent = postsInformation[index % pagingCount].PostContent;
+            // Set the image of the post to the next post's image
             postInformation.PostImage = ConvertByteArrayToBitMapImage(postsInformation[index % pagingCount].PostImage);
 
+            // Returns the colors of the likeIcon and favouriteIcon
             return iconColors;
 
         }
+
+        // Likes or unlikes a specific post and returns the color of the like icon
         public static SolidColorBrush LikePost(ref List<PostInformation> postsInformation, int index, int pagingCount)
         {
+            // If the post is not liked, like it
             if (postsInformation[index % pagingCount].IsPostLiked == false)
             {
+                // Like the specific post
                 Services.Like(postsInformation[index % pagingCount].PostId, CurrentUserInformation.CurrentUserId.Value);
+                // Set the like state of the post to liked
                 postsInformation[index % pagingCount].IsPostLiked = true;
+                // Return color DeepSkyBlue
                 return new SolidColorBrush(Colors.DeepSkyBlue);
             }
+            // If the post is liked, unlike it
             else
             {
+                // Unlike the specific post
                 Services.Unlike(postsInformation[index % pagingCount].PostId, CurrentUserInformation.CurrentUserId.Value);
+                // Set the like state of the post to unliked
                 postsInformation[index % pagingCount].IsPostLiked = false;
+                // Return color DeepSkyBlue
                 return (SolidColorBrush)new BrushConverter().ConvertFrom("#2b2b2b");
             }
         }
 
+        // Favourites or unfavourites a specific post and returns the color of the favourite icon
         public static SolidColorBrush FavouritePost(ref List<PostInformation> postsInformation, int index, int pagingCount)
         {
+            // If the post is not favourited, favourite it
             if (postsInformation[index % pagingCount].IsPostFavourited == false)
             {
+                // Favourites the specific post
                 Services.Favourite(postsInformation[index % pagingCount].PostId, CurrentUserInformation.CurrentUserId.Value);
+                // Set the favourite state of the post to favourited
                 postsInformation[index % pagingCount].IsPostFavourited = true;
+                // Return color DeepSkyBlue
                 return new SolidColorBrush(Colors.Red);
             }
+            // If the post is favourited, unfavourite it
             else
             {
+                // Unfavourites the specific post
                 Services.Unfavourite(postsInformation[index % pagingCount].PostId, CurrentUserInformation.CurrentUserId.Value);
+                // Set the favourite state of the post to unfavourited
                 postsInformation[index % pagingCount].IsPostFavourited = false;
+                // Return color DeepSkyBlue
                 return (SolidColorBrush)new BrushConverter().ConvertFrom("#2b2b2b");
             }
         }
 
+        // Follow or unfollow
         public static void FollowBlog(ref BlogContent blogContent, int blogId)
         {
+            // If the blog is not followed, follow it
             if (blogContent.IsFollowed == false)
             {
+                // Follow the specific blog
                 Services.Follow(CurrentUserInformation.CurrentUserId.Value, blogId);
+                // Set the follow state od the blog to followed
                 blogContent.IsFollowed = true;
             }
+            // Else unfollow it
             else
             {
+                // Unfollow the specific blog
                 Services.Unfollow(CurrentUserInformation.CurrentUserId.Value, blogId);
+                // Set the follow state od the blog to unfollowed
                 blogContent.IsFollowed = false;
             }
         }
 
+        // Load blogs information followed by the current user
         public static void LoadBlogsInformation(ref ObservableCollection<CurrentBlogInformation> FollowingBlogsInformation)
         {
+            // Get blogs followed by the curent user
             List<BlogInformation> followingblogsInformation = Services.GetFollowingBlogs(CurrentUserInformation.CurrentUserId.Value);
+
+            // Arrange the blogs into a list ObservableCollection<CurrentBlogInformation> FollowingBlogsInformation 
             foreach (BlogInformation followingblogInformation in followingblogsInformation)
             {
                 FollowingBlogsInformation.Add(new CurrentBlogInformation()
@@ -219,9 +286,13 @@ namespace DiabetesTracker.Logic
             }
         }
 
+        // Load tags information
         public static void LoadTagsInformation(ObservableCollection<CurrentTagInformation> TagsInformation)
         {
+            // Get tags
             List<TagInformation> tagsInformation = Services.GetTags();
+
+            // Arrange the tags into a list ObservableCollection<CurrentBlogInformation> TagsInformation 
             foreach (TagInformation tagInformation in tagsInformation)
             {
                 TagsInformation.Add(new CurrentTagInformation()
@@ -233,8 +304,10 @@ namespace DiabetesTracker.Logic
             }
         }
 
+        // Load comments information
         public static void LoadComments(ObservableCollection<CurrentCommentInformation> Comments, int postId)
         {
+            // Arrange the comments
             foreach (string comment in Services.GetComments(CurrentUserInformation.CurrentUserId.Value, postId))
             {
                 Comments.Add(new CurrentCommentInformation()
@@ -244,11 +317,14 @@ namespace DiabetesTracker.Logic
             }
         }
 
+        // Add a comment
         public static void Comment(string comment, int postId)
         {
+            // Check the comment
             if (!HandleUserInput.GeneralHandler(comment))
                 return;
 
+            // Add comment
             Services.Comment(CurrentUserInformation.CurrentUserId.Value, postId, comment);
         }
     }
--- DiabetesTracker/Logic/UserAuthentocationLogic.cs
+++ DiabetesTracker/Logic/UserAuthentocationLogic.cs
@@ -13,46 +13,60 @@ namespace DiabetesTracker.Logic
 {
     public static class UserAuthentocationLogic
     {
+        // Add user profile
         public static void FinishRegistration(UserAuthenticationWindow userAuthentication, string country, string city, string about, char gender)
         {
+            //Checks the user input
             if (!HandleUserInput.GeneralHandler(country, city, about))
                 return;
 
             try
             {
+                // Add user profile
                 Services.FinishRegistration(CurrentUserInformation.CurrentUserId.Value, gender, about, country, city);
+                // Open the main window
                 userAuthentication.OpenMainWindow();
             }
             catch (Exception exception)
             {
+                // Show error message box
                 MessageBox.Show(exception.Message, "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
         }
+
         public static void LogIn(UserAuthenticationWindow userAuthentication, string userName, string password, bool doRememberMe)
         {
+            //Checks the user input
             if (!HandleUserInput.GeneralHandler(userName, password))
                 return;
 
             try
             {
+                // Log in and assign the current user id to the id of the user that has just logged
                 CurrentUserInformation.CurrentUserId = Services.LogIn(userName, password, doRememberMe);
+                // Open the main window
                 userAuthentication.OpenMainWindow();
             }
             catch (Exception exception)
             {
+                // Show error message box
                 MessageBox.Show(exception.Message, "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
         }
+
         public static void Register(UserAuthenticationWindow userAuthentication, string userName, string email, string password)
         {
+            //Checks the user input
             if (!HandleUserInput.GeneralHandler(userName, email, password))
                 return;
 
             try
             {
+                // Register and assign the current user id to the id of the user that has just registered
                 CurrentUserInformation.CurrentUserId = Services.Register(userName, email, password);
+                // Show FinishRegistrationPage
                 userAuthentication.ShowPage(userAuthentication.FinishRegistrationPage);
             }
             catch (Exception exception)
